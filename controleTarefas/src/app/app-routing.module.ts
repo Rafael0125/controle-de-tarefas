47f4a28b0f16954a815301af7c49e47033e69442
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-03-24 20:53:40 -0300

    logica de finalização de tarefas feita

--- controleTarefas/src/app/app-routing.module.ts
+++ controleTarefas/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 import { InserirTarefaComponent } from './inserir-tarefa/inserir-tarefa.component';
 import { EditarTarefaComponent } from './editar-tarefa/editar-tarefa.component';
+import { FinalizarTarefaComponent } from './finalizar-tarefa/finalizar-tarefa.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'tela-inicial', pathMatch:'full'},
   {path:'tela-inicial',component:TelaInicialComponent},
   {path:'inserir-tarefa',component:InserirTarefaComponent},
-  {path:'editar-tarefa/:id', component:EditarTarefaComponent}
+  {path:'editar-tarefa/:id', component:EditarTarefaComponent},
+  {path: 'finalizar-tarefa/:id', component:FinalizarTarefaComponent}
 ];
 
 @NgModule({
--- controleTarefas/src/app/finalizar-tarefa/finalizar-tarefa.component.ts
+++ controleTarefas/src/app/finalizar-tarefa/finalizar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tarefa } from '../shared/tarefa.model';
+import { TarefaSevice } from '../tarefa.service';
 
 @Component({
   selector: 'app-finalizar-tarefa',
@@ -7,4 +11,47 @@ import { Component } from '@angular/core';
 })
 export class FinalizarTarefaComponent {
 
+  @ViewChild('formTarefa') formTarefa!:NgForm
+  tarefa!:Tarefa
+
+  constructor(
+    private tarefaService:TarefaSevice,
+    private route: ActivatedRoute,
+    private router: Router
+  ){ }
+
+
+  ngOnInit(): void {
+
+    let id = +this.route.snapshot.params['id']
+
+    const resposta = this.tarefaService.buscarPorId(id);
+    if(resposta !== undefined){
+      this.tarefa = resposta
+    } else{
+      throw new Error('Tarefa não encontrada: id = ' + id)
+    }
+    
+  }
+
+  finalizar($event:any,tarefa:Tarefa):void{
+    if(this.formTarefa.form.valid){
+      this.tarefaService.finalizar(this.tarefa)
+      this.tarefaService.remover(tarefa.id!);
+
+      
+      this.router.navigate(['/tela-inicial'])
+    }
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
