6bec8529cdc5e88cf160f84564aa6bd79921b097
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-03-25 23:07:53 -0300

    template e logica do componente historico-tarefa criados

--- controleTarefas/src/app/historico-tarefa/historico-tarefa.component.ts
+++ controleTarefas/src/app/historico-tarefa/historico-tarefa.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa } from '../shared/tarefa.model';
+import { TarefaSevice } from '../tarefa.service';
 
 @Component({
   selector: 'app-historico-tarefa',
   templateUrl: './historico-tarefa.component.html',
   styleUrls: ['./historico-tarefa.component.css']
 })
-export class HistoricoTarefaComponent {
+export class HistoricoTarefaComponent implements OnInit {
+
+  public tarefasFinalizadas: Tarefa[] = []
+
+
+  constructor(private tarefaService: TarefaSevice){ }
+
+  ngOnInit(): void {
+    this.tarefasFinalizadas = this.listarTarefasFinalizadas()
+  }
+
+  listarTarefasFinalizadas():Tarefa[]{
+    return this.tarefaService.listarTarefasFinalizadas()
+  }
 
 }
